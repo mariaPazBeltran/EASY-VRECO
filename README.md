5b637c21a86224d75ff91d91f1f5d22dec0906aa
María Beltrán <mariabeltra.98@gmail.com>
2020-01-14 00:50:23 -0300

    cambiando la api de google por leaflet y mapbox

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# EASY-VRECO
-Este proyecto se realizará con el objetivo de aprender a manipular la geolocalización dentro de nuestra página web, además del uso de una determinada librería.
+Este proyecto se realizará con el objetivo de aprender a manipular la geolocalización dentro de nuestra página web, además del uso de una determinada librería de js y framework de css
+### Herramientas
+- jquery
+- leaflet
+- mapbox
 ### Puntos a tratar:
-- Api de geolocalización
-- bootstrap o otro framework de css.
-- librería de javascript,
+> Note: La idea es utilizar Jquery. Pero a medida que valla avanzando puedo cambiar de librería
+> 
+- [x] Utiliza jquery o otra librería de js
+ - [x] Utiliza api de geolocalización
+ - [x] Se visualiza el mapa en la app
+ - [x] Obtiene la ubicación del usuario
+ - [x] Marca la ubicación del usuario
+ - [x] Tiene una función de error en caso de 	que falle al encontrar al usuario
+ - [ ] Trazar una ruta entre dos puntos
+ - [ ] Tiene auto-completado en los imputs
+ - [ ] El diseño debe ser realizado con grillas y utilizar un framework de css
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function initMap() {
+/*function initMap() {
     let directionsService = new google.maps.DirectionsService;
     let directionsDisplay = new google.maps.DirectionsRenderer;
     let map = new google.maps.Map(document.getElementById('map'), {
@@ -7,4 +7,32 @@ function initMap() {
         lat:  -33.431919,
         lng: -70.647897
       }
-    })}
+    })}*/
+    var mymap = L.map('map').setView([-33.431919, -70.647897], 13);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFyeS1wZWFjZSIsImEiOiJjazVkOXBqemowNDIwM2RvNndlNTBseGgxIn0.AjZcCL7JFd-Lx6oFntj-Tg', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+$('#ubication-User').on('click', (event)=>{
+  event.preventDefault()
+  console.log("aqui toy!")
+  mymap.locate({setView: true, maxZoom: 16});
+  function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(mymap)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(mymap);
+}
+mymap.on('locationfound', onLocationFound);
+function onLocationError(e) {
+  alert(e.message);
+}
+
+mymap.on('locationerror', onLocationError);
+})
